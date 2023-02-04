6fcb9a0e228d80b765fd6e228d3cfdb1f71be70b
Charles <charlespun6@gmail.com>
2023-02-04 03:25:20 -0500

    test

---
+++ BoulderingApp/Shared/Charles.swift
@@ -0,0 +1,20 @@
+//
+//  Charles.swift
+//  BoulderingApp
+//
+//  Created by Charles Pun on 2/4/23.
+//
+
+import SwiftUI
+
+struct Charles: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Charles_Previews: PreviewProvider {
+    static var previews: some View {
+        Charles()
+    }
+}
