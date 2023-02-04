b29038cbb03893d368712259ca0d6ad4e7b4e5cf
r3xley <rexley623@gmail.com>
2023-02-04 04:32:21 -0500

    c

--- BoulderingApp/Shared/SessionView/EntryView.swift
+++ BoulderingApp/Shared/SessionView/EntryView.swift
@@ -10,14 +10,23 @@ import SwiftUI
 struct EntryView: View {
     var body: some View {
         VStack {
-            
+            Spacer()
+            Text("Create a New Project:")
+                .font(.largeTitle)
+                .fontWeight(.bold)
+            Spacer()
             Circle()
-                .fill(.green)
+                .fill(.orange)
                 .frame(width: 100, height: 100)
                 .overlay(Text("+").font(.system(size: 36)).foregroundColor(Color.white))
+            Spacer()
             Button("Cancel") {
-                /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+                
+                NavigationLink(destination: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Destination@*/Text("Destination")/*@END_MENU_TOKEN@*/) {
+    
+                }
             }
+            Spacer()
         }
         
     }
--- BoulderingApp/Shared/SessionView/SwiftUIView.swift
+++ BoulderingApp/Shared/SessionView/SwiftUIView.swift
@@ -9,9 +9,22 @@ import SwiftUI
 
 struct SwiftUIView: View {
     var body: some View {
-        List {
-            
+        VStack {
+            List {
+                
+            }
+            Spacer()
+            Circle()
+                .fill(.orange)
+                .frame(width: 100, height: 100)
+                .overlay(Text("+").font(.system(size: 36)).foregroundColor(Color.white))
+            Spacer()
+            Button("End") {
+                /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+            }
+            Spacer()
         }
+       
     }
 }
 
