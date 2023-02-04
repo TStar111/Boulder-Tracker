5e82af02cbc777d86900ba2c945db9278527dfbe
r3xley <rexley623@gmail.com>
2023-02-04 05:44:14 -0500

    c

--- BoulderingApp/Shared/SessionView/EntryView.swift
+++ BoulderingApp/Shared/SessionView/EntryView.swift
@@ -9,28 +9,43 @@ import SwiftUI
 
 struct EntryView: View {
     var body: some View {
-        VStack {
-            Spacer()
-            Text("Create a New Project:")
-                .font(.largeTitle)
-                .fontWeight(.bold)
-            Spacer()
-            Circle()
-                .fill(.orange)
-                .frame(width: 100, height: 100)
-                .overlay(Text("+").font(.system(size: 36)).foregroundColor(Color.white))
+        ZStack {
+            Color(red: /*@START_MENU_TOKEN@*/0.5/*@END_MENU_TOKEN@*/, green: /*@START_MENU_TOKEN@*/0.5/*@END_MENU_TOKEN@*/, blue: /*@START_MENU_TOKEN@*/0.5/*@END_MENU_TOKEN@*/)
+                .ignoresSafeArea()
+            VStack {
+                Spacer()
+                
+                Text("Create a New Project:")
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                
+                
+                
+            
+            Button("New Project") {
+                print("Button pressed!")
+            }
+            .padding()
+            .background(.orange)
+            .foregroundColor(.white)
+            .clipShape(Capsule())
             Spacer()
             Button("Cancel") {
-                
-                NavigationLink(destination: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Destination@*/Text("Destination")/*@END_MENU_TOKEN@*/) {
-    
-                }
+                NavigationLink(destination: ContentView()) {
+                        ContentView()
+                        
             }
-            Spacer()
+            
+            
+        }
+                }
+                Spacer()
+        }
+        
         }
         
     }
-}
+
 
 struct EntryView_Previews: PreviewProvider {
     static var previews: some View {
--- BoulderingApp/Shared/SessionView/SwiftUIView.swift
+++ BoulderingApp/Shared/SessionView/SwiftUIView.swift
@@ -9,21 +9,25 @@ import SwiftUI
 
 struct SwiftUIView: View {
     var body: some View {
-        VStack {
-            List {
-                
+        ZStack {
+            Color(red: /*@START_MENU_TOKEN@*/0.5/*@END_MENU_TOKEN@*/, green: /*@START_MENU_TOKEN@*/0.5/*@END_MENU_TOKEN@*/, blue: /*@START_MENU_TOKEN@*/0.5/*@END_MENU_TOKEN@*/)
+            VStack {
+                List {
+                    
+                }
+                Spacer()
+                Circle()
+                    .fill(.orange)
+                    .frame(width: 100, height: 100)
+                    .overlay(Text("+").font(.system(size: 36)).foregroundColor(Color.white))
+                Spacer()
+                Button("End") {
+                    /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+                }
+                Spacer()
             }
-            Spacer()
-            Circle()
-                .fill(.orange)
-                .frame(width: 100, height: 100)
-                .overlay(Text("+").font(.system(size: 36)).foregroundColor(Color.white))
-            Spacer()
-            Button("End") {
-                /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
-            }
-            Spacer()
         }
+       
        
     }
 }
