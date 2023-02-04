9c0017f17ccfcc018adb8c17f3fd7166b5db3ffc
Charles <charlespun6@gmail.com>
2023-02-03 23:45:53 -0500

    initial

---
+++ BoulderingApp/Shared/BoulderingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BoulderingAppApp.swift
+//  Shared
+//
+//  Created by Charles Pun on 2/3/23.
+//
+
+import SwiftUI
+
+@main
+struct BoulderingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
