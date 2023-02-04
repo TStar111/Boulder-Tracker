56f71f196315bb1fd93d1e8e7aeb429ae99fc31d
Ricky Gao <rgao622@gmail.com>
2023-02-04 04:11:41 -0500

    Started TimeLocation, created close button view

---
+++ BoulderingApp/Shared/SummaryView/Close.swift
@@ -0,0 +1,26 @@
+//
+//  Close.swift
+//  BoulderingApp (iOS)
+//
+//  Created by Ricky Gao on 2/4/23.
+//
+
+import SwiftUI
+
+struct Close: View {
+    var body: some View {
+        Text("CLOSE")
+            .padding([.leading, .trailing], 20.0)
+            .padding([.top, .bottom], 10)
+            .background(.red)
+            .font(.headline)
+            .foregroundColor(.white)
+            .border(.black, width: 2)
+    }
+}
+
+struct Close_Previews: PreviewProvider {
+    static var previews: some View {
+        Close()
+    }
+}
